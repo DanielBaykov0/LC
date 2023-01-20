f2b699ba71034c2461e20eec4aae6a3feae0fe97
DBaykov <danibaikov@abv.bg>
2023-01-20 19:19:43 +0200

    adding Binary Search

---
+++ Study Plan Algorithm/704. Binary Search/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int target = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (target > nums[middle]) {
+                left = middle + 1;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
