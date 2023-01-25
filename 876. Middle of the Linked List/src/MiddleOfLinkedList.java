fcaf98a27a0d8979c897272de8d92314a25d349a
Daniel Baykov <danibaikov@abv.bg>
2023-01-25 19:41:49 +0200

    adding Middle of the Linked List

---
+++ 876. Middle of the Linked List/src/MiddleOfLinkedList.java
@@ -0,0 +1,37 @@
+public class MiddleOfLinkedList {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
