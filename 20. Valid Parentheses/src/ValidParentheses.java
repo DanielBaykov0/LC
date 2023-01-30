d31076985c5110b24fae5634f9621eb63c1e38a6
Daniel Baykov <danibaikov@abv.bg>
2023-01-30 18:28:10 +0200

    adding Valid Parentheses

---
+++ 20. Valid Parentheses/src/ValidParentheses.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+
+        String s = "(]";
+
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(ch);
+                    break;
+
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '}') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
