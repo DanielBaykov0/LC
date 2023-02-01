a3d5a89b86b1af2bf91e755f98a90aca34abf16e
Daniel Baykov <danibaikov@abv.bg>
2023-02-01 18:31:16 +0200

    adding Single Number

---
+++ Easy/136. Single Number/src/SingleNumber.java
@@ -0,0 +1,20 @@
+public class SingleNumber {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 2, 1};
+
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        int num = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            num ^= nums[i];
+        }
+
+        return num;
+    }
+}
