eb5cd6beda5dba2750c710c6235392d5ef272294
Daniel Baykov <danibaikov@abv.bg>
2023-02-04 10:23:47 +0200

    adding Reverse Integer

---
+++ Medium/7. Reverse Integer/src/ReverseInteger.java
@@ -0,0 +1,27 @@
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+
+        int n = 1534236469;
+
+        System.out.println(reverse(n));
+    }
+
+    public static int reverse(int x) {
+
+        int rev = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
