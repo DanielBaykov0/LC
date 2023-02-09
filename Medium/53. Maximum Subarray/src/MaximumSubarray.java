f6fcafde3aad1ac624389ef142fa7cc01dc751a0
Daniel Baykov <danibaikov@abv.bg>
2023-02-09 18:51:43 +0200

    adding Maximum Subarray

---
+++ Medium/53. Maximum Subarray/src/MaximumSubarray.java
@@ -0,0 +1,24 @@
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println(maxSubArray(nums));
+
+    }
+
+    public static int maxSubArray(int[] nums) {
+
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
